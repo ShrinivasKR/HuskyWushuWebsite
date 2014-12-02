985d24fd2b05076c48a8a49376a1cadc7d1553de
Ivy <ivy.cm.wong@gmail.com>
2014-12-02 11:59:22 -0800

    Added the basic nav bar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by Shrinivas on 11/18/2014.
  */
+
+"use strict"
+
+document.getElementById("navBar").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='History.html'>History</a></li>" +
+    "<li><a href='Events.html'>Events</a></li>" +
+    "<li><a href='Gallery.html'>Gallery</a></li>" +
+    "</ul>";
