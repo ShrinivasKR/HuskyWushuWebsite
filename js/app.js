8b4230e168ee74b0407c45715af140b643acacdd
Michael Dang <michael.w.dang@gmail.com>
2014-12-02 17:29:17 -0800

    Added dynamic page loading

--- js/app.js
+++ js/app.js
@@ -4,12 +4,24 @@
 
 "use strict"
 
-document.getElementById("navBar").innerHTML =
-    "<ul id='menu'>" +
-    "<li><a href='index.html'>Home</a></li>" +
-    "<li><a href='History.html'>History</a></li>" +
-    "<li><a href='Events.html'>Events</a></li>" +
-    "<li><a href='Gallery.html'>Gallery</a></li>" +
-    "</ul>";
-//document.getElementById("trademark").innerHTML =
-//    ""
+$(document).ready(function() {
+    //initial
+
+    //handle menu clicks
+    $('ul#nav li a').click(function() {
+        var page = $(this).attr('href');
+        $('#content').load(page + '.html');
+        return false;
+    });
+
+
+//    document.getElementById("navBar").innerHTML =
+//        "<ul id='menu'>" +
+//        "<li><a href='index.html'>Home</a></li>" +
+//        "<li><a href='History.html'>History</a></li>" +
+//        "<li><a href='Events.html'>Events</a></li>" +
+//        "<li><a href='Gallery.html'>Gallery</a></li>" +
+//        "</ul>";
+////document.getElementById("trademark").innerHTML =
+////    ""
+});
