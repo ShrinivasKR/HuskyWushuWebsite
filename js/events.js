734d067f35800bd261d1b27335954089ddc8c0a7
Ivy <ivy.cm.wong@gmail.com>
2014-12-02 16:33:02 -0800

    Made the basic parallax scrolling work for the events page

--- js/events.js
+++ js/events.js
@@ -2,5 +2,44 @@
  * Created by Ivy on 12/2/2014.
  */
 
-"use strict"
+"use strict";
+
+jQuery(document).ready(function ($){
+   $(window).stellar();
+
+    var links = $('#navigation').find('li');
+    var slide = $('.slide');
+    var myWindow = $(window);
+    var htmlbody = $('html,body');
+    var dataslide;
+
+    slide.waypoint(function (event, direction) {
+        dataslide = $(this).attr('data-slide');
+
+        if(direction == 'down') {
+            $('#navigation li[data-slide="' + dataslide +'"]').addClass('current')
+        } else {
+            $('#navigation li[data-slide="' + dataslide +'"]').addClass('current')
+        }
+    });
+
+    myWindow.scroll(function() {
+        if(myWindow.scrollTop() == 0) {
+            $('#navigation li[data-slide="1"]').addClass('current');
+            $('#navigation li[data-slide="2"]').removeClass('current');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutExpo');
+    }
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+});
 
